deefd59eb7589c9b3b9b18c1f4857ccf2b2da848
kholodoff <kholodoff@gmail.com>
2021-10-11 19:25:17 +0800

    employee

---
+++ Employee/Employee/Employee/BaseEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Employee
+{
+    abstract class BaseEmployee : IComparable<BaseEmployee>
+    {
+        protected string _name;
+        protected int _age;
+        protected double _payment;
+
+        public string Name { get { return _name; } }
+        public int Age { get { return _age; } }
+
+        public BaseEmployee(string Name, int Age, double Payment)
+        {
+            this._name = Name;
+            this._age = Age;
+            this._payment = Payment;
+        }
+        public abstract double Calculate();
+
+        public int CompareTo(BaseEmployee other)
+        {
+            return _age - other._age;
+        }
+    }
+
+
+}
---
+++ Employee/Employee/Employee/FixedWorker.cs
@@ -0,0 +1,16 @@
+﻿namespace Employee
+{
+    class FixedWorker : BaseEmployee
+    {
+        public FixedWorker(string Name, int Age, double Payment) : base(Name, Age, Payment)
+        {
+        }
+
+        public override double Calculate()
+        {
+            return _payment;
+        }
+    }
+
+
+}
