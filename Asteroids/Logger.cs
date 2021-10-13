2457f0b47dd97f511a9e2fb5fa26813128e0721f
kholodoff <kholodoff@gmail.com>
2021-10-13 17:14:10 +0800

    lesson 3

---
+++ Asteroids/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    class Logger
+    {
+        protected string _logfile = $"{Directory.GetCurrentDirectory()}\\asteroids.log";
+        protected StreamWriter _fs;
+
+        public Logger()
+        {
+            //Console.Clear();
+            _fs = File.AppendText(_logfile);
+            _fs.WriteLine("===== NEW GAME =====");
+        }
+
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+            _fs.WriteLine(text);
+        }
+    }
+}
